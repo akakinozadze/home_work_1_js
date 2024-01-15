9676839f900da40314a9f6c26795a99f8ef0a928
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-01-15 20:11:59 +0400

    done

---
+++ js/style.js
@@ -0,0 +1,25 @@
+let user1 = 100;
+if (user1 < 50) {
+console.log("less 50");
+} else if (user1 > 20) {
+    console.log("less 20");
+} else {
+    console.log("error");
+}
+
+
+let user2 = "mariami"
+let result = user2 == "mariami" ? "true" : "folse"
+console.log(result)
+
+
+switch (user2) {
+    case "mariami":
+        console.log("true");
+        break;
+
+    default : 
+       console.log ("false");
+    break;
+}
+
